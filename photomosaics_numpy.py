ea1bead8967d0d6760f5967d4afec046e3029edf
Melissa Li <li.melissa.kun@gmail.com>
2021-02-11 01:18:37 -0500

    Create plan

--- photomosaics_numpy.py
+++ photomosaics_numpy.py
@@ -3,24 +3,18 @@ from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# my_image = Image.open('doggo.jpg')
-# im.show()
-# im.size = (width, length)
-# print(im.size) 
-# convert image to numpy array
-# numpy_array = np.array(im)
-# # (height, width)
-# print(numpy_array.shape)
-
-def main():
-    my_image = io.imread('doggo.jpg')
-
 # image as numpy array
 # # (height, width, channel)
 # my_image[0:300, 0:200, :] = [255, 0, 0]
 # plt.imshow(my_image)
 # plt.show()
+
+def main():
+    my_image = io.imread('doggo.jpg')
+
+    # LOAD THE SOURCE IMAGES
+    source_images = ''
+    # source_images
 
 # LET THE USER SET THIS IN FUTURE
     square_pixel = 20
@@ -39,10 +33,35 @@ def main():
             # then crop the source images into a square and resize to the square_pixel dimensions
             # then replace that part of the original with the "new pixel"
             # done? 
+
+            pixel_region = my_image[i:i+square_pixel, j:j+square_pixel]
+            pixel_colour = calculate_average_colour(pixel_region)
+            new_tile = match_colour_resize_tile(pixel_colour, source_images)
+
+            # paste the match_colour tile into the square of the original image
+
             my_image[i:i+square_pixel, j:j+square_pixel] = [255, 0, 0]
 
     plt.imshow(my_image)
     plt.show()
+
+def calculate_average_colour(pixel_region):
+    # calculate average rgb values in that tile region
+    # return the average rgb values
+    pass
+
+def match_colour_resize_tile(pixel_colour, source_images):
+    # calculates Euclidean distance in rgb colour space
+    # gets name of smallest distance image
+    # make a copy of the tile, then resize it to the square_pixels size
+    # return to main()
+    pass
+
+def square_source_images():
+    # squares off all the source/tile images if not already squared off
+    # should happen when the program is run?
+    pass
+
 
 if __name__ == '__main__':
     main()
