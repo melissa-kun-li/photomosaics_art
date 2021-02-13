4ee409f1774ae314d0fc1a9f682cf3020d0502d6
Melissa Li <li.melissa.kun@gmail.com>
2021-02-12 22:25:59 -0500

    Created match average rgb function and resize function

--- photomosaics_numpy.py
+++ photomosaics_numpy.py
@@ -2,14 +2,17 @@ from PIL import Image
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 
-# image as numpy array
-# # (height, width, )
-# my_image[0:300, 0:200, :] = [255, 0, 0]
-# plt.imshow(my_image)
-# plt.show()
+# TO DO:
+# 1. make the pythagoras function/match the average rgbs, return the filename of the matching source image
+# 2. resize the matching source image to the size of the square_pixel
+#       img = resize(my_image, (square_pixel,square_pixel), anti_aliasing=True)
+# 3. paste the image into the location of the pic
 
 def main():
+    # LET THE USER SET THIS IN FUTURE
+    square_pixel = 50
     # load input image
     my_image = io.imread('doggo.jpg')
     # load source images, future let person decide which dir
@@ -21,7 +24,8 @@ def main():
    
     image_collection = io.imread_collection(photo_dir)
     for filepath in image_collection.files:
-        filename = filepath[len(photo_dir):]
+        filename = filepath
+        # filename = filepath[len(photo_dir)-4:]
         source_image = io.imread(filepath)
         # crop to square:
         source_images[filename] = square_source_images(source_image)
@@ -30,15 +34,12 @@ def main():
     # iterate through dictionary, setting img = the loaded image
     for source_image in source_images:
         img = source_images[source_image]
-         # set the values of the dictionary with the average rgb colour 
+ 
+        # set the values of the dictionary with the average rgb colour 
         source_image_avg_rgb_dict[filename] = calculate_average_colour(img)
 
+        # print(list(calculate_average_colour(img)))
 
-
-   
-
-# LET THE USER SET THIS IN FUTURE
-    square_pixel = 20
 
     trim_rows = my_image.shape[0] % square_pixel
     trim_columns = my_image.shape[1] % square_pixel
@@ -59,7 +60,9 @@ def main():
             pixel_colour = calculate_average_colour(pixel_region)
             # print(pixel_colour)
 
-            new_tile = match_colour_resize_tile(pixel_colour, source_images)
+            new_tile = match_colour_resize_tile(pixel_colour, source_image_avg_rgb_dict)
+
+            resized_tile = match_image_resize(new_tile, square_pixel)
 
             # paste the match_colour tile into the square of the original image
 
@@ -69,22 +72,26 @@ def main():
     plt.imshow(my_image)
     plt.show()
 
-
+# list turns [1 2 3] to [1, 2, 3] so that I can match avg rgb colours easier
 def calculate_average_colour(pixel_region):
-    return(np.mean(pixel_region, axis =(0,1)))
+    return(list(np.mean(pixel_region, axis =(0,1))))
 
 # def load_source_images():
 #     images = []
 #     image_collection = skimage.io.imread_collection('./small_photoset/*jpg')
 
 
-def match_colour_resize_tile(pixel_colour, source_images):
-    # calculates Euclidean distance in rgb colour space
-    # gets name of smallest distance image
-    # make a copy of the tile, then resize it to the square_pixels size
-    # return the name of the resized tile to main()
-    pass
-
+def match_colour_resize_tile(pixel_colour, source_image_avg_rgb_dict):
+    smallest_distance = None
+    best_match = None
+    for filename in source_image_avg_rgb_dict:
+        distance = (pixel_colour[0] - source_image_avg_rgb_dict[filename][0])**2 + (pixel_colour[1] - source_image_avg_rgb_dict[filename][1])**2 + (pixel_colour[2] - source_image_avg_rgb_dict[filename][2])**2
+        if smallest_distance == None or distance < smallest_distance:
+            smallest_distance = distance
+            best_match = filename
+    
+    return best_match
+        
 
 def square_source_images(source_img):
     # crop on the smaller, e.g. if 1280 * 800 pixels, crop to 800 * 800
@@ -100,9 +107,10 @@ def square_source_images(source_img):
         source_img = source_img[:, 0:new_width]
         return source_img
 
+def match_image_resize(new_tile, square_pixel):
+    img = io.imread(new_tile)
+    img = resize(img, (square_pixel,square_pixel), anti_aliasing=True)
+    return img
+
 if __name__ == '__main__':
     main()
-
-# my_image[0:square_pixel, 0:square_pixel, :] = [255, 0, 0]
-# plt.imshow(my_image)
-# plt.show()
